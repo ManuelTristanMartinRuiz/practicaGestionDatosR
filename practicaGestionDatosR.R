a7f932cbdf260dc4e01e6e17d36bb718de2a94b3
ManuelTristanMartinRuiz <manueltristanmartin@gmail.com>
2024-02-10 17:18:00 +0100

    Update practicaGestionDatosR.R

--- practicaGestionDatosR.R
+++ practicaGestionDatosR.R
@@ -18,7 +18,7 @@ length(unique(materiales_encontrados))
 
 # Ej.5 
 años_excavaciones <- c(2013,2015,2016,2017,2021,2022)
-length(unique(años_excavaciones))
+años_excavaciones[length(años_excavaciones)]-años_excavaciones[1]
 
 # Ej.6
 matrix1 <- matrix(c(3,4,6,5,4,2,1,4,5,3,1,2), 
@@ -30,7 +30,7 @@ which.max(rowSums(matrix1))
 matrix2 <- matrix(c(1992,1993,1996,1998,2002,2005,2007,2010,2011,2020,2008,1999), 
                   nrow = 4, ncol = 3)
 print(matrix2)
-which.min(colSums(matrix2))
+which.min(colMeans(matrix2))
 
 # Ej.8
 matrix3 <- matrix(c(23,34,15,23,17,47,35,19,14,32,27,38), 
@@ -42,7 +42,7 @@ which.max(rowSums(matrix3))
 matrix4 <- matrix(c(0.4,0.3,1,0.6,0.8,0.9,1.2,1.3,2.1,1,0.3,0.6), 
                   nrow = 4, ncol = 3)
 print(matrix4)
-which.min(rowSums(matrix4))
+which.min(rowMeans(matrix4))
 
 # Ej.10
 matrix5 <- matrix(c(23,34,15,23,17,47,35,19,14,32,27,38), 
